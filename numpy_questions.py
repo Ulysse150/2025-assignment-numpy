e7c187684241d797aae3242252608ab84e674509
Ulysse150 <raphael.monteloup@gmail.com>
2025-12-15 11:23:03 +0100

    UP  my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -20,6 +20,7 @@ import numpy as np
 
 def max_index(X):
     """Return the index of the maximum in a numpy array.
+
     Parameters
     ----------
     X : ndarray of shape (n_samples, n_features)
@@ -28,16 +29,14 @@ def max_index(X):
     Returns
     -------
     (i, j) : tuple(int)
-        The row and columnd index of the maximum.
+        The row and column index of the maximum.
 
     Raises
     ------
     ValueError
         If the input is not a numpy array or
         if the shape is not 2D.
-
     """
-
     i = 0
     j = 0
 
@@ -54,6 +53,7 @@ def max_index(X):
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
+
     See:
     https://en.wikipedia.org/wiki/Wallis_product
 
